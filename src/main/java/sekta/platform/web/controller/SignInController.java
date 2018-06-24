d2c3380e0528e8f31407b07e7ef8c1a27e02925b
vader <darthrevan1648@gmail.com>
2018-06-24 18:17:06 +0300

    need replace GET on POST

--- src/main/java/sekta/platform/web/controller/SignInController.java
+++ src/main/java/sekta/platform/web/controller/SignInController.java
@@ -3,20 +3,15 @@ package sekta.platform.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import sekta.platform.core.entity.User;
 import sekta.platform.core.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.List;
-import java.util.Scanner;
 
 @Controller
 @RequestMapping("login")
@@ -25,41 +20,36 @@ public class SignInController {
     private UserService us;
 
     @RequestMapping(value="")
-    //@ResponseBody//повертає html
     public String showAll(@ModelAttribute("user") User userWantLogin,HttpServletRequest request) throws FileNotFoundException {//цей метод в мене для входу
-        Scanner scanner = new Scanner(new File("src/main/webapp/WEB-INF/jsp/login/login.html"));
-        String html = "";
-        while (scanner.hasNextLine()) {
-            html += scanner.nextLine();
-        }
         List<User> users=us.getAllUsers();//цим рядком ми отримуємо користувачів
 
         for(User user:users){
             System.out.println(user.getUserName());
             if(user.getUserName().equals(userWantLogin.getUserName()) && user.getPassword().equals(userWantLogin.getPassword())){
-                if(user.isFlag()==true)html="Your admin";
-                else html="Your user";
-                return "redirect:/login-true";
+                if(user.isFlag())return "main";
+                return "login/login-true";
             }
         }
         return "login/login";
     }
 
-    @RequestMapping(value="login-true",method = RequestMethod.POST)//даний метод я не використовував
+    @RequestMapping(value="login-true")//даний метод я не використовував  method = RequestMethod.POST
     public String showAll(ModelMap model){
+        System.out.println("kek");
+        //model.addAttribute("login");
 
-        model.addAttribute("login");
-
-        return "login/login";
+        return "login/login-true";
     }
-    @RequestMapping(value="/login", method = RequestMethod.GET)//даний метод я не використовував
+
+
+    /*@RequestMapping(value="/login", method = RequestMethod.GET)//даний метод я не використовував
     public String login(@ModelAttribute List<User> users, RedirectAttributes redirectAttributes) {
 
         //Criteria criteriaDescription = us.getAllUsers();
         users=us.getAllUsers();
         for(User user:users) System.out.println(user.getUserName());
         return "login";
-    }
+    }*/
 
     /*@RequestMapping(value="/login", method = RequestMethod.POST)
     public String login(@ModelAttribute("loginRequest") LoginRequest loginRequest, HttpServletRequest request) {
---
+++ src/main/webapp/WEB-INF/jsp/login/login-true.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org" xmlns:spring="http://www.w3.org/1999/xhtml">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"></link>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css"></link>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <h1>Login</h1>
+    <form role="form">
+        <p>Success login</p>
+    </form>
+</div>
+</body>
+</html>
